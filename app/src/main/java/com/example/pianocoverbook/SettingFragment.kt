1c9b7b1a4dff26cca43cc5f9687b04e6141d6600
Yuki Naka <yukinaka2168@au.com>
2024-10-24 17:23:07 +0900

    「SettingFragment」内に関数「SettingScreen」に画面を構成する関数を移してからビルドしてみたら思い描いた通りのUIを実現。

--- app/src/main/java/com/example/pianocoverbook/SettingFragment.kt
+++ app/src/main/java/com/example/pianocoverbook/SettingFragment.kt
@@ -6,15 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.ComposeView
@@ -41,8 +47,38 @@ class SettingFragment : Fragment() {
 
 @Composable
 fun SettingScreen() {
-    Column {
-        Text(text = "設定画面です")
+    Row {
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SetingTextView()
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+            ) {
+                Column(
+                    modifier = Modifier
+                        .wrapContentHeight(),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    AboutThisAppLazyColumn(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                            .clip(RoundedCornerShape(16.dp))
+                            .background(color = androidx.compose.ui.graphics.Color.LightGray)
+                    )
+                }
+            }
+        }
     }
 }
 
