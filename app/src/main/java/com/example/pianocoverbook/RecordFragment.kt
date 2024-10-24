7f392feb52694b1ae6cb17b76f31e1d88415ca00
Yuki Naka <yukinaka2168@au.com>
2024-10-24 16:39:08 +0900

    「RecordFragment」内に関数「SongNameTextView」とプレビュー関数「SongNameTextViewPreview」を追加。

--- app/src/main/java/com/example/pianocoverbook/RecordFragment.kt
+++ app/src/main/java/com/example/pianocoverbook/RecordFragment.kt
@@ -7,8 +7,11 @@ import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
+import com.example.pianocoverbook.ui.theme.PianoCoverBookTheme
 
 class RecordFragment : Fragment() {
     override fun onCreateView(
@@ -29,5 +32,21 @@ fun RecordScreen() {
     // ComposeのUIを定義
     Column {
         Text(text = "記録画面です")
+    }
+}
+
+@Composable
+fun SongNameTextView(songName: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "曲名",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SongNameTextViewPreview() {
+    PianoCoverBookTheme {
+        SongNameTextView("曲名")
     }
 }
