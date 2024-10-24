1cdb969fcc61201014f36b870c5ff8766cdf51c7
Yuki Naka <yukinaka2168@au.com>
2024-10-24 17:15:17 +0900

    「SettingFragment」内に関数「SetingTextView」とそのプレビュー関数「SetingTextViewPreview」を追加。

--- app/src/main/java/com/example/pianocoverbook/SettingFragment.kt
+++ app/src/main/java/com/example/pianocoverbook/SettingFragment.kt
@@ -9,6 +9,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pianocoverbook.ui.theme.PianoCoverBookTheme
 
 class SettingFragment : Fragment() {
     override fun onCreateView(
@@ -28,5 +31,21 @@ class SettingFragment : Fragment() {
 fun SettingScreen() {
     Column {
         Text(text = "設定画面です")
+    }
+}
+
+@Composable
+fun SetingTextView() {
+    Text(
+        "設定",
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SetingTextViewPreview() {
+    PianoCoverBookTheme {
+        SetingTextView()
     }
 }
