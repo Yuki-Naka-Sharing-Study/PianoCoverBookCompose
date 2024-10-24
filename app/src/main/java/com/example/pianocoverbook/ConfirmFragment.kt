06ac2ce566e96d0139a08e183e90f366dcb5b0e8
Yuki Naka <yukinaka2168@au.com>
2024-10-24 17:27:23 +0900

    「ConfirmFragment」内に関数「NoRecordImageView」とそのプレビュー関数「NoRecordImageViewPreview」を追加。

--- app/src/main/java/com/example/pianocoverbook/ConfirmFragment.kt
+++ app/src/main/java/com/example/pianocoverbook/ConfirmFragment.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import com.example.pianocoverbook.ui.theme.PianoCoverBookTheme
 
 class ConfirmFragment : Fragment() {
     override fun onCreateView(
@@ -29,5 +37,24 @@ fun ConfirmScreen() {
     // ComposeのUIを定義
     Column {
         Text(text = "記録確認画面です")
+    }
+}
+
+@Composable
+fun NoRecordImageView(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.music_note),
+        contentDescription = "Description of the image",
+        modifier = modifier
+            .size(200.dp)
+            .aspectRatio(1f)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoRecordImageViewPreview() {
+    PianoCoverBookTheme {
+        NoRecordImageView(modifier = Modifier)
     }
 }
