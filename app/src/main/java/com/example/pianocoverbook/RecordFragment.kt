f82e710a4d98e80f63ad2f77de646c89eb79beab
Yuki Naka <yukinaka2168@au.com>
2024-10-24 16:46:30 +0900

    「RecordFragment」内に関数「SongNameTextField」とプレビュー関数「SongNameTextFieldPreview」を追加。

--- app/src/main/java/com/example/pianocoverbook/RecordFragment.kt
+++ app/src/main/java/com/example/pianocoverbook/RecordFragment.kt
@@ -1,15 +1,25 @@
 package com.example.pianocoverbook
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import com.example.pianocoverbook.ui.theme.PianoCoverBookTheme
 
@@ -48,5 +58,36 @@ fun SongNameTextView(songName: String, modifier: Modifier = Modifier) {
 fun SongNameTextViewPreview() {
     PianoCoverBookTheme {
         SongNameTextView("曲名")
+    }
+}
+
+@Composable
+fun SongNameTextField(modifier: Modifier = Modifier) {
+    OutlinedTextField(
+        modifier = modifier
+            .height(52.dp)
+            .clip(RoundedCornerShape(10))
+            .border(
+                width = 2.dp,
+                color = Color.Gray,
+                shape = RoundedCornerShape(5.dp)
+            ),
+        value = "",
+        onValueChange = { },
+        placeholder = {
+            Text(
+                text = "ラ・カンパネラ",
+                style = TextStyle(fontSize = 16.sp),
+                color = Color.Gray
+            ) },
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SongNameTextFieldPreview() {
+    PianoCoverBookTheme {
+        SongNameTextField()
     }
 }
