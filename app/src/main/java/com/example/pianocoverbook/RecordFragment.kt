faf1759facd94fe136e5e1bd7789d0c2788c7707
Yuki Naka <yukinaka2168@au.com>
2024-10-26 12:11:59 +0900

    「RecordFragment」をリファクタリング。

--- app/src/main/java/com/example/pianocoverbook/RecordFragment.kt
+++ app/src/main/java/com/example/pianocoverbook/RecordFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,7 +18,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
@@ -28,375 +26,147 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
-import com.example.pianocoverbook.ui.theme.PianoCoverBookTheme
 
+// 以下、リファクタリング後のコード
 class RecordFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val composeView = ComposeView(requireContext()).apply {
+        return ComposeView(requireContext()).apply {
             setContent {
                 RecordScreen()
             }
         }
-        return composeView
     }
 }
 
 @Composable
 fun RecordScreen() {
-    // ComposeのUIを定義
     Column(
-
+        modifier = Modifier.padding(16.dp)
     ) {
+
         Spacer(modifier = Modifier.height(16.dp))
-
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Spacer(modifier = Modifier.width(16.dp))
-            SongNameTextView("曲名")
-            Spacer(modifier = Modifier.width(16.dp))
-            SongNameTextField(Modifier.weight(1f, true))
-            Spacer(modifier = Modifier.width(16.dp))
-        }
+        InputRow(label = "曲名", placeholder = "ラ・カンパネラ")
 
         Spacer(modifier = Modifier.height(8.dp))
-
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Spacer(modifier = Modifier.width(16.dp))
-            ArtistNameTextView("アーティスト名")
-            Spacer(modifier = Modifier.width(16.dp))
-            ArtistNameTextField(Modifier.weight(1f, true))
-            Spacer(modifier = Modifier.width(16.dp))
-        }
+        InputRow(label = "アーティスト名", placeholder = "リスト")
 
         Spacer(modifier = Modifier.height(8.dp))
-
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Spacer(modifier = Modifier.width(16.dp))
-            MemoTextView("メモ")
-            Spacer(modifier = Modifier.width(16.dp))
-            MemoTextField(Modifier.weight(1f, true))
-            Spacer(modifier = Modifier.width(16.dp))
-        }
+        InputRow(label = "メモ", placeholder = "アルペジオが上手く弾けない。")
 
         Spacer(modifier = Modifier.height(8.dp))
+        ProgressSection("右手の習熟度") { RightHandCircularProgressWithSeekBar() }
+        ProgressSection("左手の習熟度") { LeftHandCircularProgressWithSeekBar() }
 
-        Row {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                RightHandTextView("右手の習熟度")
-                RightHandCircularProgressWithSeekBar()
-
-                LeftHandTextView("左手の習熟度")
-                LeftHandCircularProgressWithSeekBar()
-
                 SaveRecordButton()
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun RecordScreenPreview() {
-    PianoCoverBookTheme {
-        RecordScreen()
+fun InputRow(label: String, placeholder: String) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = label)
+        Spacer(modifier = Modifier.width(16.dp))
+        androidx.compose.material.OutlinedTextField(
+            modifier = Modifier
+                .weight(1f)
+                .height(52.dp),
+            value = "",
+            onValueChange = {},
+            placeholder = {
+                Text(
+                    text = placeholder,
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.Gray
+                )
+            },
+            shape = RoundedCornerShape(10),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Gray,
+                unfocusedBorderColor = Color.Gray
+            )
+        )
+        Spacer(modifier = Modifier.width(16.dp))
     }
 }
 
 @Composable
-private fun SongNameTextView(songName: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "曲名",
-        modifier = modifier
+fun ProgressSection(label: String, progressContent: @Composable () -> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = label)
+        progressContent()
+    }
+}
+
+@Composable
+private fun CircularProgressWithSeekBar(progress: MutableState<Float>) {
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.size(160.dp)) {
+        CircularProgressIndicator(
+            color = Color.Blue,
+            strokeWidth = 4.dp,
+            progress = progress.value / 100,
+            modifier = Modifier
+                .size(160.dp)
+                .padding(8.dp)
+        )
+        Text(text = "${progress.value.toInt()}%", fontSize = 36.sp)
+    }
+    Slider(
+        colors = SliderDefaults.colors(
+            activeTrackColor = Color.Blue,
+            inactiveTrackColor = Color.Gray,
+            thumbColor = Color.Blue
+        ),
+        value = progress.value,
+        onValueChange = { newValue -> progress.value = newValue },
+        valueRange = 0f..100f,
+        modifier = Modifier.padding(16.dp)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-private fun SongNameTextViewPreview() {
-    PianoCoverBookTheme {
-        SongNameTextView("曲名")
-    }
-}
-
-@Composable
-private fun SongNameTextField(modifier: Modifier = Modifier) {
-    OutlinedTextField(
-        modifier = modifier
-            .height(52.dp)
-            .clip(RoundedCornerShape(10))
-            .border(
-                width = 2.dp,
-                color = Color.Gray,
-                shape = RoundedCornerShape(5.dp)
-            ),
-        value = "",
-        onValueChange = { },
-        placeholder = {
-            Text(
-                text = "ラ・カンパネラ",
-                style = TextStyle(fontSize = 16.sp),
-                color = Color.Gray
-            ) },
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun SongNameTextFieldPreview() {
-    PianoCoverBookTheme {
-        SongNameTextField()
-    }
-}
-
-@Composable
-private fun ArtistNameTextView(artistName: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "アーティスト名",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun ArtistNameTextViewPreview() {
-    PianoCoverBookTheme {
-        ArtistNameTextView("アーティスト名")
-    }
-}
-
-@Composable
-private fun ArtistNameTextField(modifier: Modifier = Modifier) {
-    OutlinedTextField(
-        modifier = modifier
-            .height(52.dp)
-            .clip(RoundedCornerShape(10))
-            .border(
-                width = 2.dp,
-                color = Color.Gray,
-                shape = RoundedCornerShape(5.dp)
-            ),
-        value = "",
-        onValueChange = { },
-        placeholder = {
-            Text(
-                text = "リスト",
-                style = TextStyle(fontSize = 16.sp),
-                color = Color.Gray
-            ) },
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun ArtistNameTextFieldPreview() {
-    PianoCoverBookTheme {
-        ArtistNameTextField()
-    }
-}
-
-@Composable
-private fun MemoTextView(memoName: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "メモ",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun MemoTextViewPreview() {
-    PianoCoverBookTheme {
-        MemoTextView("メモ")
-    }
-}
-
-@Composable
-private fun MemoTextField(modifier: Modifier = Modifier) {
-    OutlinedTextField(
-        modifier = modifier
-            .height(52.dp)
-            .clip(RoundedCornerShape(10))
-            .border(
-                width = 2.dp,
-                color = Color.Gray,
-                shape = RoundedCornerShape(5.dp)
-            ),
-        value = "",
-        onValueChange = { },
-        placeholder = {
-            Text(
-                text = "アルペジオが上手く弾けない。",
-                style = TextStyle(fontSize = 16.sp),
-                color = Color.Gray
-            ) },
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun MemoTextFieldPreview() {
-    PianoCoverBookTheme {
-        MemoTextField()
-    }
-}
-
-@Composable
-private fun RightHandTextView(rightHand: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "右手の習熟度",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun RightHandTextViewPreview() {
-    PianoCoverBookTheme {
-        RightHandTextView("右手の習熟度")
-    }
-}
-
-@Composable
-private fun LeftHandTextView(leftHand: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "左手の習熟度",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun LeftHandTextViewPreview() {
-    PianoCoverBookTheme {
-        LeftHandTextView("左手の習熟度")
-    }
-}
-
-@Composable
-private fun RightHandCircularProgressWithSeekBar() {
+fun RightHandCircularProgressWithSeekBar() {
     val rightHandProgress = remember { mutableStateOf(0f) }
-
-    Box(contentAlignment = Alignment.Center,
-        modifier = Modifier.size(160.dp))
-    {
-        CircularProgressIndicator(
-            color = Color.Blue,
-            strokeWidth = 4.dp,
-            progress = rightHandProgress.value / 100,
-            modifier = Modifier
-                .size(160.dp)
-                .padding(8.dp))
-        Text(
-            text = "${rightHandProgress.value.toInt()}%",
-            fontSize = 36.sp
-        )
-    }
-
-    Slider(
-        colors = SliderDefaults.colors(
-            activeTrackColor = Color.Blue,
-            inactiveTrackColor = Color.Gray,
-            thumbColor = Color.Blue
-        ),
-        value = rightHandProgress.value,
-        onValueChange = { newValue ->
-            rightHandProgress.value = newValue
-        },
-        valueRange = 0f..100f,
-        modifier = Modifier.padding(16.dp)
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun RightHandCircularProgressWithSeekBarPreview() {
-    PianoCoverBookTheme {
-        RightHandCircularProgressWithSeekBar()
-    }
+    CircularProgressWithSeekBar(progress = rightHandProgress)
 }
 
 @Composable
-private fun LeftHandCircularProgressWithSeekBar() {
+fun LeftHandCircularProgressWithSeekBar() {
     val leftHandProgress = remember { mutableStateOf(0f) }
-
-    Box(contentAlignment = Alignment.Center,
-        modifier = Modifier.size(160.dp))
-    {
-        CircularProgressIndicator(
-            color = Color.Blue,
-            strokeWidth = 4.dp,
-            progress = leftHandProgress.value / 100,
-            modifier = Modifier
-                .size(160.dp)
-                .padding(8.dp))
-        Text(
-            text = "${leftHandProgress.value.toInt()}%",
-            fontSize = 36.sp
-        )
-    }
-
-    Slider(
-        colors = SliderDefaults.colors(
-            activeTrackColor = Color.Blue,
-            inactiveTrackColor = Color.Gray,
-            thumbColor = Color.Blue
-        ),
-        value = leftHandProgress.value,
-        onValueChange = { newValue ->
-            leftHandProgress.value = newValue
-        },
-        valueRange = 0f..100f,
-        modifier = Modifier.padding(16.dp)
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun LeftHandCircularProgressWithSeekBarPreview() {
-    PianoCoverBookTheme {
-        LeftHandCircularProgressWithSeekBar()
-    }
+    CircularProgressWithSeekBar(progress = leftHandProgress)
 }
 
 @Composable
-private fun SaveRecordButton() {
+fun SaveRecordButton() {
     Button(
         onClick = {},
         colors = ButtonDefaults.buttonColors(Color.Blue)
     ) {
         Text("記録する")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun SaveRecordButtonPreview() {
-    PianoCoverBookTheme {
-        SaveRecordButton()
     }
 }
